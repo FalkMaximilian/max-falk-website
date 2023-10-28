2ab987e3dc76c6ba7de924684c0410a8d6d0294e
Maximilian Falk <falk.maximilian@outlook.com>
2023-10-28 22:45:55 +0200

    added warnings on user authentication

--- base/views.py
+++ base/views.py
@@ -10,6 +10,27 @@ from .forms import RegisterUserForm
 HOME = 'home'
 LOGIN = 'login-user'
 
+def __check_user_and_register(request):
+    try:
+        user = User.objects.get(username=request.POST.get('username').lower())
+        if user != None:
+            messages.warning(request, "Username already in use.")
+            return
+    except User.DoesNotExist:
+        pass
+        
+    register_form = RegisterUserForm(request.POST)
+    if register_form.is_valid():
+        user = register_form.save(commit=False)
+        user.username = user.username.lower()
+        user.save()
+        login(request, user)
+        return redirect(HOME)
+    else:
+        messages.warning(request, "Password must be at least 8 characters long and contain numbers and letters. ")
+        return
+
+
 def register_user(request):
 
     if request.user.is_authenticated:
@@ -18,18 +39,30 @@ def register_user(request):
     register_form = RegisterUserForm()
 
     if request.method == 'POST':
-        register_form = RegisterUserForm(request.POST)
-        if register_form.is_valid():
-            user = register_form.save(commit=False)
-            user.username = user.username.lower()
-            user.save()
-            login(request, user)
-            return redirect(HOME)
-        else:
-            messages.warning(request, "Couldn't create account :(")
+        __check_user_and_register(request)
 
     context = {'form': register_form}
     return render(request, 'base/register_form.html', context) 
+
+
+def __check_creds_and_login(request):
+    username = request.POST.get('username').lower()
+    password = request.POST.get('password')
+
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        messages.warning(request, 'This user does not exist.')
+        return
+        
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        del password
+        login(request, user)
+        return redirect(HOME)
+    else:
+        messages.warning(request, 'Wrong password!')
+        return
 
 def login_user(request):
 
@@ -37,21 +70,7 @@ def login_user(request):
         return redirect(HOME)
 
     if request.method == 'POST':
-        username = request.POST.get('username').lower()
-        password = request.POST.get('password')
-
-        try:
-            user = User.objects.get(username=username)
-        except User.DoesNotExist:
-            messages.warning(request, 'This username does not exist.')
-        
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            del password
-            login(request, user)
-            return redirect(HOME)
-        else:
-            messages.error(request, 'Wrong password!')
+        __check_creds_and_login(request)
 
     return render(request, 'base/login_form.html', {})
 

ce2ab514c74e7ab6aef9eac50c987c4bcb60f964
Maximilian Falk <falk.maximilian@outlook.com>
2023-11-05 15:40:28 +0100

    js

--- static/js/delete_modal.js
+++ static/js/delete_modal.js
@@ -30,15 +30,21 @@ if (taskContainer) {
     })
 }
 
-var active_list = null
+var active_list = null;
 
-BuildList()
+BuildList();
 
-let lists = document.getElementsByClassName('todolist-link')
+const lists = document.querySelectorAll(".select-list-item");
+console.log(lists)
 
-for (let item of lists) {
-    console.log(item)
-}
+lists.forEach(btn => {
+    console.log(typeof(btn))
+    btn.addEventListener('click', event => {
+        alert("Hallo");
+    });
+});
+
+
 
 function BuildList() {
     const lists_wrapper = document.getElementById('lists-wrapper')
@@ -55,7 +61,7 @@ function BuildList() {
             
             var item = `
                 <li class="nav-item hstack rounded my-1 bg-body-tertiary todolist-list-item">
-                    <a class="nav-link text-break fs-5 me-2 todolist-link" style="overflow-wrap: break-word;">${list_list[i].title}</a>
+                    <a class="nav-link text-break fs-5 me-2 todolist-link" style="overflow-wrap: break-word;" list-id="${list_list[i].id}">${list_list[i].title}</a>
                     <a class="text-body-tertiary icon-link ms-auto me-2 fs-3" href="#">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
@@ -67,7 +73,7 @@ function BuildList() {
             if (active_list === null) {
                 item = `
                     <li class="nav-item hstack rounded my-1 bg-primary todolist-list-item">
-                        <a class="nav-link text-break fs-5 me-2 todolist-link text-light" href="#" style="overflow-wrap: break-word;">${list_list[i].title}</a>
+                        <a class="nav-link text-break fs-5 me-2 todolist-link text-light" style="overflow-wrap: break-word;" list-id="${list_list[i].id}">${list_list[i].title}</a>
                         <a class="text-body-tertiary icon-link ms-auto me-2 fs-3" href="#">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
